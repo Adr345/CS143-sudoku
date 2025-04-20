c346af036a4eb3a9dcd95fb6d8350b1150401197
Adr345 <158979207+Adr345@users.noreply.github.com>
2025-04-20 16:24:33 -0700

    Complete the reading of board date file and constructor

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,3 +1,15 @@
-public class SudokuBoard{
+import java.util.*;
+import java.io.*;
 
+public class SudokuBoard{
+   private int[][] board;
+   
+   public SudokuBoard(String fileName) throws FileNotFoundException {
+      board = new int[9][9];
+      Scanner input = new Scanner(new File(fileName));
+   }
+   
+   public String toString(){
+      return "";
+   }
 }
