67377be3c09e698cc99caa79ddb25b63b71e8e81
Adr345 <158979207+Adr345@users.noreply.github.com>
2025-04-20 16:32:18 -0700

    Completed reading the board data file and the constructor

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -2,14 +2,23 @@ import java.util.*;
 import java.io.*;
 
 public class SudokuBoard{
-   private int[][] board;
+   private char[][] board;
    
    public SudokuBoard(String fileName) throws FileNotFoundException {
-      board = new int[9][9];
+      board = new char[9][9];
       Scanner input = new Scanner(new File(fileName));
+      while(input.hasNextLine()){
+         for(int r = 0; r < board.length; r++){
+            String line = input.nextLine();
+            for(int c = 0; c < board[r].length; c++){
+               board[r][c] = line.charAt(c);
+            }
+         }
+      }
    }
    
    public String toString(){
-      return "";
+      String board = "";
+      return board;
    }
 }
